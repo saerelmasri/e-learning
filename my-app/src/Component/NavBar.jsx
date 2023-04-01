d0a62678e502ca787fe0056f75858e0be954395d
SaerElMasri <saer1890@gmail.com>
2023-04-01 15:12:25 +0200

    Added links to the NavBar

--- my-app/src/Component/NavBar.jsx
+++ my-app/src/Component/NavBar.jsx
@@ -1,11 +1,23 @@
 import Button from '../Component/Button';
+import { useNavigate } from 'react-router-dom';
 const NavBar = () => {
+    const navigator = useNavigate();
+
+    const discoverNavigate = () => {
+        navigator('/dashboard');
+    }
+    const courseNavigate = () => {
+        navigator('/enrolled_courses');
+    }
+    const filesNavigate = () => {
+        navigator('/files');
+    }
     return(
         <nav>
                 <div className='nav-links-container'>
-                    <h3>Discover</h3>
-                    <h3>Enrolled Course</h3>
-                    <h3>Files</h3>
+                    <h3 onClick={discoverNavigate}>Discover</h3>
+                    <h3 onClick={courseNavigate}>Enrolled Course</h3>
+                    <h3 onClick={filesNavigate}>Files</h3>
                 </div>
                 <div className='nav-btn-continer'>
                     <Button title={'Log Out'}/>
