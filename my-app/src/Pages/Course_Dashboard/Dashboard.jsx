dbeb639e01dad141c848eedf91900fc56ef2f4b5
SaerElMasri <saer1890@gmail.com>
2023-04-01 18:59:12 +0200

    Added fetch courses api to frontend

--- my-app/src/Pages/Course_Dashboard/Dashboard.jsx
+++ my-app/src/Pages/Course_Dashboard/Dashboard.jsx
@@ -2,16 +2,38 @@
 import NavBar from '../../Component/NavBar';
 import Course from '../../Component/Course';
 import './dashboard.css'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 const Dashboard = () => {
-    const hello = () => {
-        console.log('Hello');
+    const [ response, setResponse ] = useState([]);
+    const token = localStorage.getItem('token');
+
+    const fetchCourses = async() => {
+        await axios({
+            method:'GET',
+            url: 'http://localhost:5000/course/allCourses',
+            headers: {
+                'Authorization': token
+            }
+        }).then(res => {
+            setResponse(res.data.courses)
+        }).catch(err => {
+            console.error(err);
+        })
     }
+
+    useEffect(()=>{
+        fetchCourses()
+    }, []);
+
     return(
        <div className='dashboard-bg'>
             <NavBar/>
             <div className='course-box-container'>
                 <div className='course-flexbox-container'>
-                    <Course course_code={'CSI 201'} course_title={'Intro to Programming'} course_instructor={'Charbel Boustany'} action={hello}/>
+                    {response.map((res) => {
+                        return <Course key={res.id} course_code={res.course_code} course_title={res.course_name} course_instructor={res.instructor}/>
+                    })}
                 </div>
             </div>
        </div>
