6861596b534016c70ff9cb73073512a6983087bf
SaerElMasri <saer1890@gmail.com>
2023-04-02 20:02:46 +0200

    Added main.js file

---
+++ admin-app/main.js
@@ -0,0 +1,46 @@
+const { app, BrowserWindow, Menu } = require('electron');
+const path = require('path');
+
+const isMac = process.platform === 'darwin';
+
+
+function createMainWindow() {
+    const mainWindow = new BrowserWindow({
+        title: 'Admin Dashboard',
+        width: 1200,
+        height: 900,
+        webPreferences: {
+            contextIsolation:true,
+            nodeIntegration: true,
+            preload: path.join(__dirname, 'preload.js'),
+        },
+    });
+
+
+    //mainWindow.loadFile(path.join(__dirname, './renderer/index.html'));
+}
+
+app.whenReady().then(() => {
+    createMainWindow();
+
+    // const mainMenu = Menu.buildFromTemplate(menu);
+    // Menu.setApplicationMenu(mainMenu);
+
+    app.on('activate', () => {
+        if(BrowserWindow.getAllWindows().length === 0){
+            createMainWindow();
+        }
+    });
+});
+
+// const menu = [
+//     {
+//         role: 'fileMenu',
+//     },
+// ]
+
+app.on('window-all-closed', () => {
+    if(!isMac){
+        app.quit();
+    }
+})
