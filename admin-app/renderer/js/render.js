9b23ca67a48b8eba70547afce07f0d0a159ddb7e
SaerElMasri <saer1890@gmail.com>
2023-04-02 20:04:59 +0200

    Added folder that includes all logic and styling

---
+++ admin-app/renderer/js/render.js
@@ -0,0 +1,57 @@
+const adminEmail = document.querySelector('#email');
+const adminPassword = document.querySelector('#password');
+
+document.querySelector('#loginAction').onclick = (e) => {
+    e.preventDefault();
+    if(adminEmail.value === '' || adminPassword === ''){
+        alertError('Plase Provide all required fields');
+    }
+    fetch('http://localhost:5000/auth/login', {
+        method: 'post',
+        headers: {
+            Accept: 'application.json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            'email': adminEmail.value,
+            'password': adminPassword.value
+        })
+    }).then((res) => {
+        return res.json();
+    }).then(data => {
+        if(data.status === 201){
+            alertSuccess('Login Successfully');
+            localStorage.setItem('token', data['token']);
+            window.location.href = 'dashboard.html';
+        }else{
+            alertError(data['message']);
+        }
+    }).catch(err => {
+        console.error(err);
+    });
+    
+} 
+const alertError = (message) => {
+    Toastify.showToast({
+        text: message,
+        duration: 5000,
+        close: false,
+        style: {
+          background: 'red',
+          color: 'white',
+          textAlign: 'center'
+        }
+    });
+}
+const alertSuccess = (message) => {
+    Toastify.showToast({
+        text: message,
+        duration: 5000,
+        close: false,
+        style: {
+          background: 'green',
+          color: 'white',
+          textAlign: 'center'
+        }
+    });
+}
