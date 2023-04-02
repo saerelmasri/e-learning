dc0077f670612613cab663f642d28327cc125041
SaerElMasri <saer1890@gmail.com>
2023-04-03 00:22:59 +0200

    Added new API to fetch students from course and finished course list page

---
+++ admin-app/renderer/js/render_list_course.js
@@ -0,0 +1,37 @@
+const JWT_TOKEN = window.localStorage.getItem('token');
+
+const fetchCourses = async() => {
+    await fetch('http://localhost:5000/course/allCourses', {
+        method: 'get',
+        headers: {
+            'Authorization': JWT_TOKEN,
+            Accept: 'application.json',
+            'Content-Type': 'application/json'
+        }
+    })
+    .then((res) => {
+        return res.json();
+    }).then(data => {
+        const courses = data['courses'];
+        courses.forEach(course => {
+            const markup = `<div class="class-component">
+                <h1>${course['course_name']}</h1>
+                <button class="class-btn" data-id="${course['_id'] }">View Class</button>
+            </div>`
+            const element = document.createRange().createContextualFragment(markup);
+            document.querySelector(".container-courses").appendChild(element);
+        });
+        const view_students = document.querySelectorAll(".class-btn");
+        view_students.forEach((btn) => {
+            btn.addEventListener("click", () => {
+                const courseID = btn.getAttribute("data-id");
+                window.localStorage.setItem('course_id',courseID);
+                window.location.href = '/details.html';
+            });
+        });
+    }).catch(err => {
+        console.error(err);
+    });
+}
+
+fetchCourses();
